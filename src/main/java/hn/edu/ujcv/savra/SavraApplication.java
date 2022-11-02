1014dd200f6e990ee2a7c38c195ea8c09058fb2b
Ricoy Pagoaga <Ricoy_Pagoaga01@outlook.com>
2022-11-02 17:15:46 -0600

    start

---
+++ src/main/java/hn/edu/ujcv/savra/SavraApplication.java
@@ -0,0 +1,13 @@
+package hn.edu.ujcv.savra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/edu/ujcv/savra/SavraApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.edu.ujcv.savra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
