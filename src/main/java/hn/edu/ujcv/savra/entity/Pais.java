2a1fda5a73773371dc27fc70a02d0bc61b449364
Ricoy Pagoaga <Ricoy_Pagoaga01@outlook.com>
2022-11-12 23:32:13 -0600

    pais,cliente

--- src/main/java/hn/edu/ujcv/savra/entity/Pais.java
+++ src/main/java/hn/edu/ujcv/savra/entity/Pais.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -15,7 +13,9 @@ import javax.persistence.Table;
 @Table(name = "pais")
 public class Pais {
     @Id
-    private long cod_iso;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idPais;
+    private String cod_iso;
     private String nombre;
     private String cod_area;
 }
